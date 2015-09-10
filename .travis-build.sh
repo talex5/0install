6ed0a946e9d5b1b44f28bd2bea522107ed0cd7c6
Thomas Leonard <talex5@gmail.com>
2015-09-10 14:02:17 +0100

    Test with OCaml-using 0compile

--- .travis-build.sh
+++ .travis-build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash -eux
 eval `opam config env`
 make
-cd ocaml
-./0install run -c http://0install.net/2006/interfaces/0compile.xml autocompile http://0install.net/tests/GNU-Hello.xml
-./0install run -c http://0install.net/tests/GNU-Hello.xml
+sudo make install
+0install add-feed http://0install.net/tools/0install.xml dist/0install/feed.xml
+git clone https://github.com/0install/0compile.git -b use-ocaml
+0install run -c 0compile/0compile.xml autocompile http://0install.net/tests/GNU-Hello.xml
+0install run -c http://0install.net/tests/GNU-Hello.xml
