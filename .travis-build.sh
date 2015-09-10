57c4d885120761115d2650681a8e1690ecdf629a
Thomas Leonard <talex5@gmail.com>
2015-09-10 09:30:03 +0100

    Travis: test 0compile after successful build

--- .travis-build.sh
+++ .travis-build.sh
@@ -1,3 +1,6 @@
 #!/bin/bash -eux
 eval `opam config env`
 make
+cd ocaml
+./0install run -c http://0install.net/2006/interfaces/0compile.xml autocompile http://0install.net/tests/GNU-Hello.xml
+./0install run -c http://0install.net/tests/GNU-Hello.xml
